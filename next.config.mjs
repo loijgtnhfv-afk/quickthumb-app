37c6fc7a0e58e535571c1d6f2ba17d4d650a8ca6
loijgtnhfv-afk <loijgtnhfv@gmail.com>
2026-05-17 22:41:01 +0900

    fix: include noto fonts in serverless bundle

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    images: {
-        remotePatterns: [
-              { protocol: "https", hostname: "i.ytimg.com" },
-                    { protocol: "https", hostname: "*.supabase.co" },
-                          { protocol: "https", hostname: "replicate.delivery" },
-                              ],
-                                },
-                                };
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.ytimg.com' },
+      { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: 'https', hostname: 'replicate.delivery' },
+    ],
+  },
+  outputFileTracingIncludes: {
+    '/api/generate': [
+      './node_modules/@fontsource/noto-sans-jp/files/*.woff2',
+      './node_modules/@fontsource/noto-serif-jp/files/*.woff2',
+    ],
+  },
+};
 
-                                export default nextConfig;
-                                
+export default nextConfig;
